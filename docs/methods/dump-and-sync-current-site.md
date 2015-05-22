66ef4fb11a62a72114691075a01fd9b9428a76d7
Anselm Christophersen <ac@anselm.dk>
2015-05-22 22:57:28 +0200

    keeping a configurable amount of backups on the backup server

--- docs/methods/dump-and-sync-current-site.md
+++ docs/methods/dump-and-sync-current-site.md
@@ -6,7 +6,16 @@
 If the backup server has been properly configured as described in the README,
 you can set a cron job up like this:
 
-	30 0,4,8,12,16,20 * * * /ABSOLUTE_REPO_PATH/ttools/backups/lib/dump-and-sync-current-site.sh
+	30 0,4,8,12,16,20 * * * /ABSOLUTE_REPO_PATH/ttools/backups/lib/dump-and-sync-current-site.sh THE_ENV_THIS_IS_RUN_ON
+
 
 Of course try it out before you just let cron do it's thing.
 This one will sync every 4 hours, but you can set things up as you please.
+
+## Configuration options
+
+You can configure the following on the environment this will be run on
+
+    Backups:
+      RemoteBackupPath: "ABSOLUTE_BACKUP_PATH"
+      RemoteBackupKeep: NUMBER_OF_BACKUPS_TO_KEEP
--- lib/dump-and-sync-current-site.sh
+++ lib/dump-and-sync-current-site.sh
@@ -39,12 +39,15 @@ eval `$ENVVARS`
 DIR_LOCAL="$BASEDIR/temp/dumps/latest/";
 
 #Server dir
-DIR_SERVER="~/latest-sync";
+DIR_SERVER="~/sync";
 #if a specific server dir has been set for the environment, use that instead
 backupPathToEval="Environments_"$FROM_ENV"_Backups_RemoteBackupPath"
 if [ "${!backupPathToEval}" != "" ]; then
 	DIR_SERVER="${!backupPathToEval}";
 fi
+
+#appending "latest" to the sync
+DIR_SERVER_LATEST=$DIR_SERVER"/latest";
 
 
 echo "Now dumping and syncing";
@@ -52,10 +55,44 @@ echo "Now dumping and syncing";
 $BASEDIR/ttools/sitesync-core/lib/dump-current-site.sh dump;
 
 
-RSYNC_CMD="$DIR_LOCAL $ENV_SSHUSER@$ENV_HOST:$DIR_SERVER"
+RSYNC_CMD="$DIR_LOCAL $ENV_SSHUSER@$ENV_HOST:$DIR_SERVER_LATEST"
 
 if [ "$ENV_CUSTOM_RSYNCPORTSTR" == "" ]; then
 	rsync -avz $RSYNC_CMD
 else
 	rsync -avz -e "$ENV_CUSTOM_RSYNCPORTSTR" $RSYNC_CMD
 fi
+
+
+# Connecting to the server, and making sure x backups are being kept
+
+DUMP_NAME=$(date +"%Y-%m-%d_%H-%M%Z");
+
+KEEP=24;
+#if a keep amount has been set for the environment, use that instead
+keepPathToEval="Environments_"$FROM_ENV"_Backups_RemoteBackupKeep"
+if [ "${!keepPathToEval}" != "" ]; then
+	KEEP="${!keepPathToEval}";
+fi
+
+echo "Done syncing, now creating a copy with datestamp, and cleaning up (keeping $KEEP dumps)"
+
+#regulating...
+KEEP=$(($KEEP+1));
+
+#from http://stackoverflow.com/questions/6024088/linux-save-only-recent-10-folders-and-delete-the-rest
+DELETE_STR="cd $DIR_SERVER/dumps; ls -dt */ | tail -n +$KEEP | xargs rm -rf;"
+
+SERVER_COMMANDS="cp -r $DIR_SERVER_LATEST $DIR_SERVER/dumps/$DUMP_NAME;"
+
+
+
+#echo $SERVER_COMMANDS;
+#echo $DELETE_STR;
+
+#http://thornelabs.net/2013/08/21/simple-ways-to-send-multiple-line-commands-over-ssh.html#using-ssh-with-the-bash-command
+ssh $ENV_CUSTOM_SSHCONNECTIONSTR bash -c "'
+$SERVER_COMMANDS
+$DELETE_STR
+'"
+
